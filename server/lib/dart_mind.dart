10f8fd14197101e52c8dcc3a52733ab6aba1e649
kevinchrist <kevinamevor@outlook.com>
2025-03-25 13:46:22 +0000

    Remove unused string extensions and position utilities from DartMind library exports

--- server/lib/dart_mind.dart
+++ server/lib/dart_mind.dart
@@ -4,5 +4,3 @@ library dart_mind;
 export 'src/models/complexity_metrics.dart';
 export 'src/analyzers/code_complexity_analyzer.dart';
 export 'src/services/document_service.dart';
-export 'src/utils/string_extensions.dart';
-export 'src/utils/position_utils.dart';
